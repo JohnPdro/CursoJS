9056dfbe4304d57127fc67feda65c86d995160e1
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-05-12 15:33:33 -0300

    Praticando JavaScript

---
+++ aula14/ambiente.js
@@ -0,0 +1,11 @@
+/*
+var c = 1 
+while (c <= 6){
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+for(c=1 ; c<=10 ; c++){
+    console.log(`Passo ${c}`)
+}
---
+++ aula14ex/ex16/script.js
@@ -0,0 +1,35 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+
+    if(ini.value.leght == 0 || fim.value.leght == 0 || passo.value.leght == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else{
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if ( i < f){
+            // Contagem crescente
+            for(let c = i ; c <= f; c += p ){
+                res.innerHTML += `${c} 👉`
+            }
+        }
+        else {
+            // Contagem regressiva
+            for(let c = i; c >= f ; c -= p){
+                res.innerHTML += `${c} 👉`
+            }
+        }
+        res.innerHTML += `🏁`
+
+    }
+    
+}
